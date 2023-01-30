31fa65219fa0809805faf8be32160233e82f46bf
Dewashishkumar <dewashishkumar42@gmail.com>
2023-01-30 17:49:31 +0530

    Simple interest calculator

--- src/Simple/Interest.js
+++ src/Simple/Interest.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import styled from "styled-components";
-import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
